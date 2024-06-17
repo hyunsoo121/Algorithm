0f593f4bf4e5a94f7e41a8bb090a4ab0fd6e0101
ggom121@naver.com <ggom121@naver.com>
2024-06-18 04:41:27 +0900

    초기설정

---
+++ baekjoon/Main.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.println("test");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
