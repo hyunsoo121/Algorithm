1d486a3086421159bd7ac7cc06b3051e0c0149e8
hyunsoo121 <ggom121@naver.com>
2024-06-19 07:12:52 +0900

    2588

---
+++ baekjoon/_2588/Main.java
@@ -0,0 +1,33 @@
+package baekjoon._2588;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        int c = a*(b%10);
+        int d = a*((b%100)/10);
+        int e = a*(b/100);
+
+        bw.write(String.valueOf(c));
+        bw.newLine();
+        bw.write(String.valueOf(d));
+        bw.newLine();
+        bw.write(String.valueOf(e));
+        bw.newLine();
+        bw.write(String.valueOf(c + (d*10) + (e*100)));
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+    
+}
