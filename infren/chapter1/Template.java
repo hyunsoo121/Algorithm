8722534ecd756eba86f5e84b454ec54d3201233c
ggom121@naver.com <ggom121@naver.com>
2024-10-22 15:40:52 +0900

    add template

---
+++ infren/chapter1/Template.java
@@ -0,0 +1,18 @@
+package infren.chapter1;
+
+import java.util.Scanner;
+
+public class Template {
+    public static int soulution(){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(soulution());
+    }
+}
