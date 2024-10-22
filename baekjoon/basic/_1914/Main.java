bdf8a5aae16280e52e23b49e7e96750aa3802fb8
ggom121@naver.com <ggom121@naver.com>
2024-10-22 15:16:04 +0900

    init 초기 세팅

--- baekjoon/basic/_1914/Main.java
+++ baekjoon/basic/_1914/Main.java
@@ -9,21 +9,21 @@ import java.math.BigInteger;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BigInteger res = new BigInteger("1");
 
         int N = Integer.parseInt(br.readLine());
-        
-        if (N<=20) {
+
+        if (N <= 20) {
             hanoi(N, 1, 3, 2);
-            sb.insert(0, (int)(Math.pow(2, N) - 1) + "\n");
+            sb.insert(0, (int) (Math.pow(2, N) - 1) + "\n");
             System.out.print(sb);
-        }
-        else {
-            for(int i=0; i<N; i++){ 
+        } else {
+            for (int i = 0; i < N; i++) {
                 res = res.multiply(new BigInteger("2"));
             }
             res = res.subtract(new BigInteger("1"));
@@ -35,18 +35,17 @@ public class Main {
 
     }
 
-    public static void move(int n, int start, int end) throws IOException{
+    public static void move(int n, int start, int end) throws IOException {
         sb.append(start + " " + end + "\n");
     }
 
-    public static void hanoi(int n, int start, int end, int via) throws IOException{
-        if (n==1)
+    public static void hanoi(int n, int start, int end, int via) throws IOException {
+        if (n == 1)
             move(1, start, end);
         else {
-            hanoi(n-1, start, via, end);
+            hanoi(n - 1, start, via, end);
             move(n, start, end);
-            hanoi(n-1, via, end, start);
+            hanoi(n - 1, via, end, start);
         }
-       
     }
 }
---
+++ infren/chapter1/_1/Main.java
@@ -0,0 +1,21 @@
+package infren.chapter1._1;
+
+import java.util.*;
+
+public class Main {
+    public static int soulution(String str, char t){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        char c = sc.next().charAt(0);
+
+        System.out.print(soulution(str, c));
+    }
+}
