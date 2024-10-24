cd2caaeaf78e3cee6f8d31dd1eb7681ee351e753
ggom121@naver.com <ggom121@naver.com>
2024-10-24 13:34:20 +0900

    1-4

---
+++ infren/chapter1/_4/Main.java
@@ -0,0 +1,18 @@
+package infren.chapter1._4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int solution(){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(solution());
+    }
+}
