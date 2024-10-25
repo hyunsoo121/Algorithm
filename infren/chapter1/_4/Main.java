fe74c2c19e3987e7fed0df059a624b98b4681373
ggom121@naver.com <ggom121@naver.com>
2024-10-25 18:02:40 +0900

    1-4(수정)

--- infren/chapter1/_4/Main.java
+++ infren/chapter1/_4/Main.java
@@ -1,18 +1,33 @@
 package infren.chapter1._4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static int solution(){
-        int answer = 0;
+    public static char[] solution(String s){
 
-        return answer;
+        int idx = s.length()-1;
+        char[] arr = s.toCharArray();
+        for (int i = 0; i <s.length()/2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[idx-i];
+            arr[idx-i] = temp;
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print(solution());
+        int n = sc.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            System.out.println(solution(s));
+        }
+
     }
 }
