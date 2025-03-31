76dce57d250cbe7119ecf934bac411d255d5af13
ggom121@naver.com <ggom121@naver.com>
2025-03-31 09:07:11 +0900

    2557

--- baekjoon/basic/_2557/Main.java
+++ baekjoon/basic/_2557/Main.java
@@ -6,13 +6,12 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write("Hello World!");
 
-        bw.flush();
-        bw.close();
-        br.close();
+        bw.write("Hello World!"); // 괄호안의 값 출력
+
+        bw.flush(); // 버퍼안에 남아있는 데이터 비우고 출력
+        bw.close(); // 버퍼안에 데이터 비우고 출력한 뒤 스트림 닫힘
     }
 }
