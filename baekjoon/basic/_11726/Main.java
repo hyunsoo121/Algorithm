eb171286ec247938d3958eb23c2bc4c966614779
ggom121@naver.com <ggom121@naver.com>
2025-09-30 13:18:49 +0900

    b_11726

---
+++ baekjoon/basic/_11726/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.basic._11726;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 2];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        bw.write(String.valueOf(dp[n]));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
